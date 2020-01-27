5e68f3b612bd4252648003404f0e28a7e953501a
prog <prog@pm-g.ru>
2020-01-27 16:18:49 +0300

    Добавил категории через круд

---
+++ backend/views/shop/category/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model shop\entities\shop\Category */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="category-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'meta_json')->textInput() ?>
+
+    <?= $form->field($model, 'lft')->textInput() ?>
+
+    <?= $form->field($model, 'rgt')->textInput() ?>
+
+    <?= $form->field($model, 'depth')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/shop/category/_search.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\forms\CategorySearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="category-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'slug') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?php // echo $form->field($model, 'meta_json') ?>
+
+    <?php // echo $form->field($model, 'lft') ?>
+
+    <?php // echo $form->field($model, 'rgt') ?>
+
+    <?php // echo $form->field($model, 'depth') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
