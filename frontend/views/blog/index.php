e0eb20322d3527a5b9d4506c84cc78e78974fab6
prog <prog@pm-g.ru>
2020-03-13 14:00:32 +0300

    ФРОНТЕНД Добавлены вывод демо статей из TJournal одключен composer require tzurbaev/osnova-api

--- frontend/views/blog/index.php
+++ frontend/views/blog/index.php
@@ -1,5 +1,18 @@
 <?php
 
-echo file_get_contents('https://pmg.ru/uslugi/');
+use Osnova\TJournal;
+use Osnova\Services\Timeline\Owners\TimelineCategory;
 
-?>
+// Create resource instance with the "1.4" API version.
+$tjournal = TJournal::make('1.4');
+
+$entries = $tjournal->getTimelineEntries(new TimelineCategory('index'));
+
+foreach ($entries as $entry) {
+    //var_dump($entry);
+    echo '<h1>'.$entry->getTitle().'</h1> --';
+    echo $entry->getIntroInFeed() ? 'yes' : 'NO' ;
+    echo '--<p>'.$entry->getIntro().'</p>';
+    echo "<hr>";
+}
+//var_dump($entries);
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,12 +11,14 @@ class ComposerStaticInite50bb6a871eec6376e517505596b987f
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         'def43f6c87e4f8dfd0c9e1b1bab14fe8' => __DIR__ . '/..' . '/symfony/polyfill-iconv/bootstrap.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
         '538ca81a9a966a6716601ecf48f4eaef' => __DIR__ . '/..' . '/opis/closure/functions.php',
         '180092cfc969a12e06f2132a203a3184' => __DIR__ . '/..' . '/codeception/verify/src/Codeception/function.php',
@@ -98,11 +100,14 @@ class ComposerStaticInite50bb6a871eec6376e517505596b987f
         ),
         'O' => 
         array (
+            'Osnova\\' => 7,
             'Opis\\Closure\\' => 13,
         ),
         'G' => 
         array (
             'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
         ),
         'F' => 
         array (
@@ -301,6 +306,10 @@ class ComposerStaticInite50bb6a871eec6376e517505596b987f
         array (
             0 => __DIR__ . '/..' . '/phpspec/prophecy/src/Prophecy',
         ),
+        'Osnova\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tzurbaev/osnova-api/src',
+        ),
         'Opis\\Closure\\' => 
         array (
             0 => __DIR__ . '/..' . '/opis/closure/src',
@@ -308,6 +317,14 @@ class ComposerStaticInite50bb6a871eec6376e517505596b987f
         'GuzzleHttp\\Psr7\\' => 
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
         'Faker\\' => 
         array (
