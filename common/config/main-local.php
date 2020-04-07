c01a98010019534c4cbb955d7dbb14d74a8deaca
svejooo <svejooo@gmail.com>
2020-04-07 03:29:15 +0300

    добавил локальное подключение к базе в гитигнор

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
             'username' => 'root',
-            'password' => '',
+            'password' => 'newqwe',
             'charset' => 'utf8',
         ],
         'mailer' => [
